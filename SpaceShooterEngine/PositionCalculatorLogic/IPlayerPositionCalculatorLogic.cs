602c549ad7158c9558f08c1bc031050fa4fb1770
Pawel Wisniewski <pawel.wis93@gmail.com>
2023-04-04 15:52:15 +0200

    Player logic implemented

---
+++ SpaceShooterEngine/PositionCalculatorLogic/IPlayerPositionCalculatorLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+namespace SpaceShooterEngine.PositionCalculatorLogic;
+
+public enum PlayerMovement
+{
+    NO_MOVEMENT,
+    LEFT,
+    RIGHT
+}
+
+public interface IPlayerPositionCalculatorLogic
+{
+    public Point GetNewPosition(Point currentPosition, PlayerMovement movement);
+}
